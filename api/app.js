652458417e23eb56d10aed9441cd2400346df5ee
weshallbe1423 <vishalnikalje23@gmail.com>
2021-03-20 12:40:56 +0530

    code to redis added

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ const cors=require('cors');
 const bodyParser=require('body-parser');
 const createError=require('http-errors');
 require('./config/initMongodb');
+require('./config/initRedis');
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
