76f73ba8682a650c52371fb553ef58cf63c3eae3
weshallbe1423 <vishalnikalje23@gmail.com>
2021-03-22 13:46:48 +0530

    serving app using express

--- api/app.js
+++ api/app.js
@@ -4,15 +4,28 @@ const app=express();
 const cors=require('cors');
 const bodyParser=require('body-parser');
 const createError=require('http-errors');
+const readFileSync=require('fs');
 require('./config/initMongodb');
 require('./config/initRedis');
 app.use(logger('dev'));
 app.use(cors());
+const path=require('path');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-
+// const staticPath=path.join(__dirname,'./public');
+app.use(express.static(path.join(__dirname,'./public/ecommerce/')));
 require('./routes/user.route')(app);
+// api\public\ecommerce
+
+app.get('/', (req,res) => {
+    res.sendFile(express.static(path.join(__dirname,'./public/ecommerce/index.html')))
+  });
+  
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 //error handling 404
 app.use((req,res,next)=>{
     next(createError.NotFound());
