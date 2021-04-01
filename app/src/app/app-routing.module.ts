99cadc489bfeaa2a9b92e54188278b2d957f4c9e
weshallbe1423 <vishalnikalje23@gmail.com>
2021-04-01 09:27:36 +0530

    routing added in the app

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'/home',component:HomeComponent
+  },
+  {
+    path:'/products',loadChildren:()=>import('./products/products.module')
+    .then(m=>m.ProductsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
